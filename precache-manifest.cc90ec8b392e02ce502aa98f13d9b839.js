b1b1e0a96142cdeda3652476b9c2c72d4a23b75c
nadiiabilockon <11071995nad@gmail.com>
2020-04-15 18:07:22 +0300

    Updates

---
+++ precache-manifest.cc90ec8b392e02ce502aa98f13d9b839.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed5cf974f132b3957562a7caea552209",
+    "url": "/jobs_search/index.html"
+  },
+  {
+    "revision": "47b7ee83e64df045e815",
+    "url": "/jobs_search/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "a383c5b9be1c7ff56b88",
+    "url": "/jobs_search/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "47b7ee83e64df045e815",
+    "url": "/jobs_search/static/js/2.24b15ca7.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/jobs_search/static/js/2.24b15ca7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a383c5b9be1c7ff56b88",
+    "url": "/jobs_search/static/js/main.4ae6069c.chunk.js"
+  },
+  {
+    "revision": "cca0e8a920a045de7b58",
+    "url": "/jobs_search/static/js/runtime-main.ed1be1e7.js"
+  }
+]);
